18941b36c353bb83b636ca983202ea0ce0c7d1a8
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2025-03-06 23:49:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-//your JS code here. If required.
+let output = document.getElementById("output");
+let arr = [1, 2, 3, 4];
+
+const myPromise = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(arr);
+        }, 3000);
+    });
+};
+
+myPromise()
+    .then((data) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                let filteredArr = data.filter((item) => item % 2 === 0);
+                output.textContent = filteredArr.join(", "); 
+                resolve(filteredArr);
+            }, 1000);
+        });
+    })
+    .then((filteredArr) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                let multipliedArr = filteredArr.map((item) => item * 2);
+                output.textContent = multipliedArr.join(", "); 
+                resolve(multipliedArr);
+            }, 2000);
+        });
+    });
